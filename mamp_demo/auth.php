d52a4f9b3f5c37fee9e05c325ce98460178adf6a
captainCwangrz <18501397628@163.com>
2025-07-27 22:05:16 +0800

    Added basic pages
    login, register, basic dashboard with basic vis net

---
+++ mamp_demo/auth.php
@@ -0,0 +1,59 @@
+<?php
+    require 'config.php';
+    $action = $_POST["action"] ?? "";
+    $username = trim($_POST["username"]) ?? "";
+    $password = $_POST["password"] ?? "";
+    if($action === "login")
+    {
+        $query = $pdo->prepare('SELECT id, password_hash FROM users WHERE username=?');
+        $query -> execute([$username]);
+        $user = $query->fetch();
+        if($user && password_verify($password, $user["password_hash"]))
+        {
+            //Store locally session
+            $_SESSION["user_id"] = $user["id"];
+            $_SESSION["username"] = $username;
+            header("Location: dashboard.php");exit;
+        }
+        header("Location: index.php?error=1");exit;
+    }
+    if($action === "register")
+    {
+        if(!$username || !$password)
+        {
+            exit("Missing field!");
+        }
+        
+        
+        $password_hash = password_hash($password, PASSWORD_DEFAULT);
+
+        // Prepare SQL statement
+        $sql = 'INSERT INTO users (username, password_hash) VALUES (?, ?)';
+        $stmt = $pdo->prepare($sql);
+
+        try 
+        {
+            // Execute query
+            $stmt->execute([$username, $password_hash]);
+        } 
+        catch (PDOException $e) 
+        {
+            if($e -> getCode() === "23000")
+            {
+                exit("Username exists");
+            }
+            throw $e;
+        }
+        header("Location: index.php?registered=1");exit;
+
+    }
+
+
+
+
+
+
+
+
+
+
---
+++ mamp_demo/dashboard.php
@@ -0,0 +1,116 @@
+<?php
+	require "config.php";
+
+	if(!isset($_SESSION["user_id"]))
+	{
+		header("Location: index.php");exit;
+	}
+    $nodes = $pdo->query('SELECT id, username FROM users')->fetchAll();
+    $edges = $pdo->query('SELECT from_id, to_id, type FROM relationships')->fetchAll();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gossip Chain</title>
+    <script src="https://unpkg.com/vis-network@latest/standalone/umd/vis-network.min.js"></script>
+    <link  href="https://unpkg.com/vis-network@latest/styles/vis-network.min.css" rel="stylesheet" type="text/css">
+        <style>
+            body
+            {
+                font-family: system-ui, sans-serif;
+                background-color: lightgreen;
+            }
+            html, body
+            {
+                margin: 0;
+                padding: 0;
+                height: 100%;
+                overflow: hidden;
+            }
+            #graph
+            {
+                width: 100vw;
+                height: 100vh;
+                border: none;
+            }
+            .box
+            {
+                max-width: 400px;
+                margin: 6rem auto;
+                padding: 1.5rem 2rem;
+                border-radius: 5px;
+                box-shadow: 0 2px 8px #000200;
+            }
+            label
+            {
+                display: block;
+                margin: 0.75rem 0;
+                margin-bottom: 0.5rem;
+            }
+            input
+            {
+                width: 100%;
+                padding: 0.5rem;
+            }
+            button
+            {
+                border-radius: 5px;
+                cursor: pointer;
+                background-color: lightyellow;
+                width: 100%;
+                padding: 0.5rem;
+                margin-left: 0.5rem;
+            }
+            hr
+            {
+                margin: 2rem 0;
+                border: none;
+                border-top: 1px solid black;   
+            }
+            .right
+            {
+                text-align: right;
+                margin: 5rem 2rem 0 0;
+            }
+            #top_bar
+            {
+                background-color: lightyellow;
+                padding: 1rem;
+                height: 30px;
+            }
+            #graph
+            {
+                height: calc(100vh-30px);
+            }
+        </style>
+</head>
+<body>
+    <div id="top_bar">
+        Hello, <?php echo htmlspecialchars($_SESSION["username"]);?><br>
+        <a href="logout.php">Log Out</a>
+    </div>
+    <div id="graph"></div>
+    <script>
+        console.log("Nodes:", <?php echo json_encode($nodes); ?>);
+        console.log("Edges:", <?php echo json_encode($edges); ?>);
+        const nodes = new vis.DataSet(<?php echo json_encode(array_map(fn($u)=>["id"=>$u["id"], "label"=>$u["username"]], $nodes));?>);
+        const edges = new vis.DataSet(<?php echo json_encode(array_map(fn($e)=>["from"=>$e["from_id"], "to"=>$e["to_id"], "label"=>$e["type"], "arrows"=>""], $edges));?>);
+        const network = new vis.Network(document.getElementById("graph"), {nodes, edges}, {physics:{enabled:true, barnesHut:{springLength: 150}}, interaction:{hover:true}});
+        network.fit();
+        window.addEventListener("resize", ()=>network.fit());
+        const user_id = <?= (int) $_SESSION["user_id"];?>;
+        network.once("stabilizationIterationsDone", function(){
+            network.focus(user_id, {
+                scale:1.5,
+                animation:{
+                    duration: 800,
+                    easingFunction: "easeInOutQuad"
+                }
+            });
+        });
+    </script>
+</body>
+</html>
